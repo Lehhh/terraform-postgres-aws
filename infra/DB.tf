90bb20f4f8da76d88f303ffe61cf0075311a2b35
Leandro Shiniti Tacara <lst@MacBook-Pro-de-Leandro.local>
2024-10-01 20:04:07 -0300

    add actions to deploy

--- infra/DB.tf
+++ infra/DB.tf
@@ -3,6 +3,7 @@ resource "aws_db_instance" "postgres" {
   engine  = "postgres"
   engine_version = "12.18"
   instance_class = var.instance_type
+  db_name = var.db_name
   username = var.db_user
   password = var.db_password
   port = var.db_port
